d6f5161fb5a631bb54a22e956cca4307c0c751b3
Alex Basystyi <alexbasisty@gmail.com>
2021-03-17 14:44:32 +0100

    starting with app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import "./App.css";
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./index.css";
 
-import { Navigation } from "./components";
+import { Navigation } from "components";
+
+import theme from "utils/theme";
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <div className="App">
+        <Navigation items={[]} />
+      </div>
+    </ThemeProvider>
   );
 }
 
